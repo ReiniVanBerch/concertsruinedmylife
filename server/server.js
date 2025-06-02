862919ef0f97ed9e917f47e9a63dff6f3b82c6e3
Florian Koller <koller.florian@gmail.com>
2025-06-02 21:22:56 +0200

    Swagger integrated and configured to display the current apis

--- server/server.js
+++ server/server.js
@@ -1,17 +1,19 @@
 require('dotenv').config()
 
 const bcrypt = require('bcrypt');
-
-
 const express = require('express');
 const session = require('express-session');
-
 const path = require('path');
 const app = express();
+
+// --- Swagger Integration ---
 const swaggerUi = require('swagger-ui-express');
+const YAML = require('yamljs'); // Use the yamljs library
+const swaggerDocument = YAML.load('./swagger.yaml'); // Load the external yaml file
 
 const { emitWarning } = require('process');
 
+// Internal API and Database modules
 const fetchEvents = require('./internal/api/fetchevents.js');
 const formatEvents = require('./internal/api/formatEvents.js');
 const fetchFlights = require('./internal/api/fetchFlights.js');
@@ -30,30 +32,23 @@ const putEvent = require('./internal/database/events/putEvent.js');
 const getEvent = require('./internal/database/events/getEvent.js');
 const deleteEvent = require('./internal/database/events/deleteEvent.js');
 
-
-
-
-//const swaggerDocument = require('./swagger.yaml');
-
-//app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
-
+// --- Middleware Setup ---
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument)); // Use the loaded document
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'files')));
 app.use(express.urlencoded({ extended: true }));
 app.use(session({
-  secret: process.env.SESSION_SECRET,
-  resave: false,
-  saveUninitialized: true,
-  cookie: { secure: false }
+    secret: process.env.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: false } // Set to true if using https
 }));
 
 
 
-
-
-
 app.get('/flights', async function (req, res) {
-    res.send((await formatFlight(await fetchFlights("VIE", "LHR", '2025-06-13', '2025-06-27',3,4))));
+    const { from, to, departDate, returnDate, adults, children } = req.query;
+    res.send((await formatFlight(await fetchFlights(from, to, departDate, returnDate, adults, children))));
 });
 
 app.get('/accomodations', async function (req, res) {
@@ -82,9 +77,6 @@ app.get('/eventdetails/:eventID', async function (req, res) {
     res.send(formatEventDetails(await eventDetails(req.params.eventID)));
 })
 
-
-
-
 //UserThingos
 //Login and Register
 app.post('/login', async (req, res) => {
@@ -102,27 +94,27 @@ app.post('/register', async (req, res) => {
 
 app.get('/auth', (req, res) => {
     if (req.session.username) {
-        res.status(200).send("Everything good!"); 
-    }else{
+        res.status(200).send("Everything good!");
+    } else {
         res.status(402).send('notPermitted');
     }
 });
 
 //See if loggedIn, and if username
-app.get('/profile', (req, res) => {ensureAuthenticated(req, res, ()=> {res.redirect(302, '/profile.html');});});
+app.get('/profile', (req, res) => { ensureAuthenticated(req, res, () => { res.redirect(302, '/profile.html'); }); });
 
 //Get Events assigned to User
-app.get('/profile/events', async function (req, res) {    ensureAuthenticated(req, res, allEvents);})
+app.get('/profile/events', async function (req, res) { ensureAuthenticated(req, res, allEvents); })
 
 //Get Specific Evetn assigned to user
-app.get('/profile/events/:event', async function (req, res) {    ensureAuthenticated(req, res, getEvent);})
+app.get('/profile/events/:event', async function (req, res) { ensureAuthenticated(req, res, getEvent); })
 
 
 //Add an event to user
-app.put('/profile/events', async function (req, res) {    ensureAuthenticated(req, res, putEvent);})
+app.put('/profile/events', async function (req, res) { ensureAuthenticated(req, res, putEvent); })
 
 //Remove an event to user
-app.delete('/profile/events/:event', async function (req, res) {    ensureAuthenticated(req, res, deleteEvent);})
+app.delete('/profile/events/:event', async function (req, res) { ensureAuthenticated(req, res, deleteEvent); })
 
 //Add a costfactor to an event
 app.put('/profile/events/:event/:costfactor', async function (req, res) {
@@ -140,8 +132,8 @@ app.delete('/profile/events/:event/:costfactor', async function (req, res) {
     res.status(501).send("Yet to be implemented!, but right location");
 })
 
+// --- Server Start ---
 app.listen(3000, () => {
     console.log("Server now listening on http://localhost:3000/");
+    console.log("API documentation available at http://localhost:3000/api-docs");
 });
-
-
