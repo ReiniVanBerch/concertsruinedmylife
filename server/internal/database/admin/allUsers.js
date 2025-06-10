35e9b1ea2deeb9d1a9c1f68cd734acf6893b2ada
ReiniVanBerch <vincent.berchi@outlook.com>
2025-06-10 13:38:24 +0200

    retitleling

--- server/internal/database/admin/allUsers.js
+++ server/internal/database/admin/allUsers.js
@@ -5,7 +5,7 @@ function allUsers(req, res){
     let getUsers = db.prepare("SELECT * FROM users");
     let users = getUsers.all();
 
-    if(users.length == 0){
+    if(users.length > 0){
         res.status(200).send(users);
     } else{
         res.status(200).send({message:"No Users found"});
